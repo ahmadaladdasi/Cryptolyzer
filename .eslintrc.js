1ef21afc0fa41c54c7632f567e6fef1f22d4bad9
ahmadaladdasi <ahmadaladdasi88@gmail.com>
2018-05-19 11:57:41 -0700

    Configured AirBnB Linter and Added needed Dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+/**
+ * These rules enforce the Hack Reactor Style Guide
+ *
+ * Visit this repo for more information:
+ *   https://github.com/reactorcore/eslint-config-hackreactor
+ */
+
+module.exports = {
+  extends: 'airbnb-base',
+  env: {
+    browser: true,
+    node: true,
+  }
+};
